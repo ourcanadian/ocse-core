7f51111e1adaf62ac99fe1cd5241e45e3eae1d54
Mek Obchey <aobchey00@uvic.ca>
2020-06-24 00:01:20 -0700

    Modified robots_txt.py to make a simple spider that crawl download either XML or JSON from a website. When deployed, search "robots.txt" for "shopify" string and if found, download "products.json". Otherwise, download the XML links it first encounters (not the XML of links to products).

--- coast_to_coast/coast_to_coast/spiders/robots_txt.py
+++ coast_to_coast/coast_to_coast/spiders/robots_txt.py
@@ -1,11 +1,44 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import json
+import os, glob
+import re
 
 class RobotsTxtSpider(scrapy.Spider):
-    name = 'robots_txt'
-    allowed_domains = ['example.com']
-    start_urls = ['http://example.com/']
+	name = 'robots_txt'
+	allowed_domains = ['muttonheadstore.com']
+	start_urls = ['https://muttonheadstore.com/']
+	folder = "scraped"
 
-    def parse(self, response):
-        pass
+	def parse(self, response):
+		robot_url = response.url + "robots.txt"
+		# prepare output folder
+		if not os.path.exists(self.folder):
+			os.remove
+			os.makedirs(self.folder)
+		else:
+			filelist = glob.glob("./" + self.folder + "/*")
+			for f in filelist:
+				os.remove(f)
+
+		if re.search("shopify", response.text, re.IGNORECASE) != None:
+			url = response.url + "products.json"
+			return scrapy.http.Request(url=url, callback=self.downloadJSON)
+		else:
+			url = response.url + "sitemap.xml"
+			return scrapy.http.Request(url=url, callback=self.downloadXML)
+
+	def downloadJSON(self, response):
+		with open(self.folder + "/" + self.domain(response.url), "w") as file:
+			data = json.loads(response.text)
+			json.dump(data, file, indent=2)
+			file.close()
+
+	def downloadXML(self, response):
+		filepath = self.folder + "/" + self.domain(response.url)
+		with open(filepath, "w") as file:
+			file.write(response.text)
+			file.close()
+
+	def domain(self, url):
+		return url.split("/")[2]
